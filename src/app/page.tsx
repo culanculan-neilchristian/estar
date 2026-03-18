5df1aef5d70ef368f279d7e774ea3ffbf050b04f
Maric <maric@prosperna.com>
2026-03-18 23:19:32 +0800

    feat: Add initial Home page component with Hero, ImpactTracker, and other sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Hero from '@/components/home/Hero';
 import ImpactTracker from '@/components/home/ImpactTracker';
 import ExpectationsReality from '@/components/home/ExpectationsReality';
