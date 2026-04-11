eca7c9d9044d2a2a3d8a3b0be2500df8bca3b7d6
Maric17x <mauroricardomarfil@gmail.com>
2026-04-11 14:47:16 +0800

    feat: initialize Payload CMS with PostgreSQL adapter and user collection

---
+++ src/app/(payload)/admin/[[...segments]]/not-found.tsx
@@ -0,0 +1,15 @@
+import { NotFoundPage } from '@payloadcms/next/views'
+import config from '@/payload.config'
+
+type Args = {
+  params: Promise<{
+    segments: string[]
+  }>
+  searchParams: Promise<{
+    [key: string]: string | string[]
+  }>
+}
+
+const NotFound = ({ params, searchParams }: Args) => NotFoundPage({ config, params, searchParams })
+
+export default NotFound
